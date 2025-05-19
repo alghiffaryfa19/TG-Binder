00eb1a5cbf8e9e7a62aa50121cf5ec58d5ae2a86
Fauzan Amir Al Ghiffary <56854433+alghiffaryfa19@users.noreply.github.com>
2025-05-19 17:06:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,45 @@
 require("dotenv").config();
-const { TelegramClient } = require("telegram");
+const { TelegramClient, Api } = require("telegram");
 const { StringSession } = require("telegram/sessions");
+const { NewMessage } = require('telegram/events');
 const input = require("input");
+const axios = require('axios');
 
 const apiId = parseInt(process.env.API_ID);
 const apiHash = process.env.API_HASH;
 const stringSession = new StringSession(process.env.STRING_SESSION);
 
-//* The channel from which the content will be taken
-const sourceChannelId = "ENTER_YOUR_VALUE";
-//* The channel where the content will be redirected to
-const finalChannelId = "ENTER_YOUR_VALUE";
+
 
 (async () => {
-  console.log("Loading...");
   const client = new TelegramClient(stringSession, apiId, apiHash, {
     connectionRetries: 5,
   });
-  await client
-    .start
-    //* Authorization in the client
-    // {
-    //   phoneNumber: async () => await input.text("Please enter your phone number: "),
-    //   password: async () => await input.text("Please enter your password: "),
-    //   phoneCode: async () =>
-    //     await input.text("Please enter the code you received: "),
-    //   onError: (err) => console.log(err),
-    // }
-    ();
 
-  //* Get your stringSession
-  // console.log(client.session.save())
-
-  console.log("You should now be connected.");
-
-  //* Get all id of all channels in your client
-  // const dialogs = await client.getDialogs();
-  // dialogs.forEach((dialog) => {
-  //   if (dialog.isChannel) {
-  //     console.log(dialog.title);
-  //     console.log(dialog.id);
-  //   }
-  // });
-
-  client.addEventHandler(async (event) => {
-    if (event.message?.peerId?.channelId?.value === sourceChannelId) {
-      let msg = event.message.message;
-      console.log(msg);
-
-      await client.sendMessage(finalChannelId, { message: msg });
-    }
+  await client.start({
+    phoneNumber: async () => await input.text('Nomor HP: '),
+    password: async () => await input.text('Password 2FA: '),
+    phoneCode: async () => await input.text('Kode OTP: '),
+    onError: (err) => console.log(err),
   });
+
+  console.log('Client siap!');
+
+
+  // Listen semua pesan masuk
+client.addEventHandler(async (event) => {
+    const message = event.message;
+
+    // Filter: hanya dari bot tertentu
+    if (message.peerId?.userId?.value == 1636695583n) {
+      try {
+        await axios.post('https://tradelogic.id/api/bot', {
+          message: message.message,
+        });
+      } catch (err) {
+        console.error('Gagal forward:', err);
+      }
+    }
+  }, new NewMessage({}));
+
 })();
